d4b40aec546e86a3a0c70a9f03cd05618db636ea
Nestor Alejandro <00856217@red.unid.mx>
2026-02-26 21:42:21 -0500

    nuevo sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@
  * - ES7+ React/Redux/React-Native snippets
  */
 //archivo: src/components/Sidebar.jsx
-
+//nuevo sidebar
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { 
