231cb9ef94512ce6b6b5110e01ea369734f214e0
arlinchc <arlinchc@gmail.com>
2026-04-17 12:05:55 -0500

    Use DATABASE_URL for Render

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,11 @@
-module.exports = pool;
-
-require('dotenv').config();
-const { Pool } = require('pg');
+require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: process.env.PGUSER || process.env.DB_USER || 'postgres',
-    host: process.env.PGHOST || process.env.DB_HOST || 'localhost',
-    database: process.env.PGDATABASE || process.env.DB_NAME || 'onTimeClock',
-    password: process.env.PGPASSWORD || process.env.DB_PASSWORD || 'postgres',
-    port: Number(process.env.PGPORT || process.env.DB_PORT || 5432),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
